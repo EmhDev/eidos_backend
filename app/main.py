d578ed6c7407d794f6ae70a981dedbd257140ea9
EmhDev <emhdev11@gmail.com>
2025-04-10 19:16:47 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
+import sys
+import os
+
+# Aseguramos que el directorio 'app' esté en el PYTHONPATH
+sys.path.append(os.path.join(os.path.dirname(__file__), 'app'))
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from api.routes import router  # Si tus rutas están definidas en otro archivo, importarlas aquí.
+from api.routes import router  # Esto debería funcionar ahora
 
 app = FastAPI()
 
