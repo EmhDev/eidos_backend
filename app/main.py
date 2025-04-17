42928c1528436f2bf935b42e4cf49862b397bcb9
EmhDev <emhdev11@gmail.com>
2025-04-17 00:49:17 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,19 +1,16 @@
 # app/main.py
 import sys
 import os
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes import router
 
 app = FastAPI()
 
-from fastapi.middleware.cors import CORSMiddleware
-
 # Configuración de CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://cleanmind-frontend.onrender.com"],  # ⚠️ el origen de tu frontend
+    allow_origins=["https://cleanmind-frontend.onrender.com"],  # Origen del frontend
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
